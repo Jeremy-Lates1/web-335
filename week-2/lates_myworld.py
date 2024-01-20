f55c85a44031f4df7e8743e0e3058eebcdff9413
Jeremy Lates <jlates@my365.bellevue.edu>
2024-01-20 06:19:01 -0500

    my first python program

---
+++ week-2/lates_myworld.py
@@ -0,0 +1,9 @@
+#
+#    Title: lates_mywold.py
+#    Author: Jeremy Lates
+#    Date: 01/20/2024
+#
+#    Code is adapted from https://www.w3schools.com/python/python_variables.asp
+
+msg = "You are now in " + "Lates's" + " world!"
+print(msg)

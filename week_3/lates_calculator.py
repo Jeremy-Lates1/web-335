8e9c6756b48368b3d56e418afea2887b3e73a92d
Jeremy Lates <jlates@my365.bellevue.edu>
2024-01-27 07:05:30 -0500

    Adding excercise 3.3 Python Variables and Functions

---
+++ week_3/lates_calculator.py
@@ -0,0 +1,40 @@
+#
+#    Title: lates_calculator.py
+#    Author: Jeremy Lates
+#    Date: 01/27/2024
+#
+#    Code is adapted from https://www.w3schools.com/python/python_functions.asp
+#    Code is adapted from https://www.w3schools.com/python/python_howto_add_two_numbers.asp
+
+#add function
+def add(value1, value2):
+  return value1+value2
+
+#subtract function
+def subtract(value1, value2):
+  return value1-value2
+
+#divide function
+def divide(value1,value2):
+  return value1/value2
+
+#multiple function 
+def multiple(value1,value2):
+  return value1*value2
+
+#set the variables with operational function
+x = add(4,4)
+
+y= subtract(10,6)
+
+z= divide(8,2)
+
+a=multiple(10,2)
+
+#Show the results
+print ("4 + 4 is ", x)
+print ("10 - 6 is ", y)
+print ("8 / 2 is ", z)
+print ("10 * 2 is ", a)
+
+
